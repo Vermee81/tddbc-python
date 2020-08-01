591b8936aec587bdc14912ae11ed73448547acd5
Hiroo Kusaba <hrksb5029@gmail.com>
2020-08-01 15:42:31 +0900

    [refactor]: PEPに注意されたのでisに変更した

--- test/test_closed_range.py
+++ test/test_closed_range.py
@@ -8,17 +8,14 @@ def test_下端点3と上端点8を渡したら文字列を返す():
 
 def test_下端点3と上端点8の閉区間に5が含まれていること():
     cr = ClosedRange(3, 8)
-    # PEPでは注意されているが、仕様のわかりやすさのために==Trueとする
-    assert cr.is_contains(5) == True
+    assert cr.is_contains(5) is True
 
 
 def test_下端点3と上端点8の閉区間に2が含まれていないこと():
     cr = ClosedRange(3, 8)
-    # PEPでは注意されているが、仕様のわかりやすさのために==Falseとする
-    assert cr.is_contains(2) == False
+    assert cr.is_contains(2) is False
 
 
 def test_下端点3と上端点8の閉区間に9が含まれていないこと():
     cr = ClosedRange(3, 8)
-    # PEPでは注意されているが、仕様のわかりやすさのために==Falseとする
-    assert cr.is_contains(9) == False
+    assert cr.is_contains(9) is False
