98fb2fb3ee157804266345a420dd9ea5eb50121b
Hiroo Kusaba <hrksb5029@gmail.com>
2020-08-01 13:56:06 +0900

    [add] クラスを定義して、文字列を返す

--- test/test_dummy.py
+++ test/test_dummy.py
@@ -1,5 +1,20 @@
 from src.calculate import add
 
 
-def test_add():
-    assert add(2, 3) == 6
+class ClosedRange:
+    def __init__(self, lower_endpoint, upper_endpoint):
+        self.lower_endpoint = lower_endpoint
+        self.upper_endpoint = upper_endpoint
+
+    def notation(self):
+        return f"[{self.lower_endpoint},{self.upper_endpoint}]"
+
+
+def test_下端点3と上端点8を渡したら文字列を返す():
+    cr = ClosedRange(3, 8)
+    assert cr.notation() == "[3,8]"
+
+
+def test_下端点4と上端点10を渡したら文字列を返す():
+    cr = ClosedRange(4, 10)
+    assert cr.notation() == "[4,10]"
