22988aabcead69fea3eb660a9df37eaee91490ee
Jun Nakajima <ikuta1919@gmail.com>
2020-08-01 16:20:21 +0900

    [refactor]: 粒度を揃えた

--- test/test_closed_range.py
+++ test/test_closed_range.py
@@ -1,14 +1,16 @@
 from closed_range import ClosedRange
 
 
-class TestClosedRange:
+class FixtureClass:
     def setup(self):
         self.cr = ClosedRange(3, 8)
 
+
+class TestClosedRange(FixtureClass):
     def test_下端点3と上端点8を渡したら文字列を返す(self):
         assert self.cr.notation() == "[3,8]"
 
-    class Test判定系:
+    class Test指定した整数で判定(FixtureClass):
         def setup(self):
             self.cr = ClosedRange(3, 8)
 
@@ -20,6 +22,8 @@ class TestClosedRange:
 
         def test_下端点3と上端点8の閉区間に9が含まれていないこと(self):
             assert self.cr.is_contains(9) is False
+
+    class Test別の閉区間と判定(FixtureClass):
 
         def test_下端点3と上端点8の閉区間と別の下端点3と上端点8が等価であること(self):
             assert self.cr == ClosedRange(3, 8)
