9e9680a10b518246d24709f56bab1e703018f378
Jun Nakajima <ikuta1919@gmail.com>
2020-08-01 15:53:07 +0900

    [refactor]: ネストクラスを使って、判定系はまとめた

--- test/test_closed_range.py
+++ test/test_closed_range.py
@@ -1,21 +1,24 @@
 from closed_range import ClosedRange
 
 
-def test_下端点3と上端点8を渡したら文字列を返す():
-    cr = ClosedRange(3, 8)
-    assert cr.notation() == "[3,8]"
+class TestClosedRange:
+    def setup(self):
+        self.cr = ClosedRange(3, 8)
+
+    def test_下端点3と上端点8を渡したら文字列を返す(self):
+        assert self.cr.notation() == "[3,8]"
+
+    class Test判定系:
+        def setup(self):
+            self.cr = ClosedRange(3, 8)
+
+        def test_下端点3と上端点8の閉区間に5が含まれていること(self):
+            assert self.cr.is_contains(5) is True
 
 
-def test_下端点3と上端点8の閉区間に5が含まれていること():
-    cr = ClosedRange(3, 8)
-    assert cr.is_contains(5) is True
+        def test_下端点3と上端点8の閉区間に2が含まれていないこと(self):
+            assert self.cr.is_contains(2) is False
 
 
-def test_下端点3と上端点8の閉区間に2が含まれていないこと():
-    cr = ClosedRange(3, 8)
-    assert cr.is_contains(2) is False
-
-
-def test_下端点3と上端点8の閉区間に9が含まれていないこと():
-    cr = ClosedRange(3, 8)
-    assert cr.is_contains(9) is False
+        def test_下端点3と上端点8の閉区間に9が含まれていないこと(self):
+            assert self.cr.is_contains(9) is False
