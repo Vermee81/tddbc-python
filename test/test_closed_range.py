b760d1e2eb13709e482a31f5223d352cc7383cb4
Jun Nakajima <ikuta1919@gmail.com>
2020-08-01 14:33:57 +0900

    判定するテストを書いた(RED)

--- test/test_closed_range.py
+++ test/test_closed_range.py
@@ -4,3 +4,7 @@ from closed_range import ClosedRange
 def test_下端点3と上端点8を渡したら文字列を返す():
     cr = ClosedRange(3, 8)
     assert cr.notation() == "[3,8]"
+
+def test_下端点3と上端点8の閉区間に5が含まれている():
+    cr = ClosedRange(3, 8)
+    assert cr.is_contains(5) == True
